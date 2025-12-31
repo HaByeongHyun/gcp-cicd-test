82b70f40823c7545263513783e076d64c6324fa2
Ha <the9616@gamil.com>
2025-12-31 14:53:28 +0900

    cicd pr test

---
+++ src/app/history/page.tsx
@@ -0,0 +1,11 @@
+import { memo } from "react";
+
+const HistoryPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="text-xl font-bold">This is History Page</div>
+    </div>
+  );
+};
+
+export default memo(HistoryPage);
